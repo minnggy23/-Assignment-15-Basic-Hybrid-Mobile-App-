3a63685df9811c22ffbbb6e6ca95ab121939c82f
minnggy23 <rubyshemaria@gmail.com>
2024-12-17 18:46:09 -0800

    Tab 1 Complete

--- photo-gallery/src/pages/Tab1.tsx
+++ photo-gallery/src/pages/Tab1.tsx
@@ -1,16 +1,32 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonListHeader, IonItem, IonLabel } from '@ionic/react';
 import React, {useState, useEffect} from 'react';
 import './Tab1.css';
+
+function parseACF(obj: any): any {
+  // loop thru each object in array returned as JSON
+  console.log('in',obj);
+  obj.forEach(
+    function(item: any): any {
+      let x = '{"' + item.acf_fields + '"}';
+      x = x.replaceAll(',','","');
+      x = x.replaceAll(':','":"');
+      let y = JSON.parse(x);
+      item.acf_fields = y;
+    }
+  );
+  ///console.log('out',obj);
+  return obj;
+}
 const Tab1: React.FC = () => {
   const [dataset, setDataset] = useState<any[]>([]);
-  const dataURL = "https://dev-srjc-fall-2024-cs55-13.pantheonsite.io/wp-json/twentytwentyone-child/v1/special";
-  
+  const dataUrl = "https://dev-srjc-fall-2024-cs55-13.pantheonsite.io/wp-json/twentytwentyone-child/v1/things";
+
   useEffect(() => {
-    fetch(dataURL) 
-    .then(response => response.json()) 
+    fetch(dataUrl) 
+    .then(response => response.json())
     .then(data => setDataset(data)) 
   },[])
-  
+  console.log({dataset})
   return (
     <IonPage>
       <IonHeader>
@@ -24,7 +40,6 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Posts</IonTitle>
           </IonToolbar>
         </IonHeader>
-      
         <IonList id="thing-list">
           <IonListHeader>Things</IonListHeader>
           {dataset.map((item, index) => (
@@ -32,8 +47,10 @@ const Tab1: React.FC = () => {
               <IonLabel>
                 <h4>{item.post_title}</h4> <br></br>
              <br></br>  <p>{item.thing_description}</p>
-                <address>{item.thing_address}</address>
-                <h6>{item.acf_fields}</h6>
+                <p>{item.thing_address}</p>
+                <img alt="Silhouette of mountains" src="https://dev-srjc-fall-2024-cs55-13.pantheonsite.io/wp-content/uploads/2024/12/bulletin-board-e1734415354724.png" />
+
+
               </IonLabel>
             </IonItem>
           ))}
